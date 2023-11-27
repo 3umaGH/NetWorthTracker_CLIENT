43f443d046e0820e252460c71ed3c709e133d428
3umaGH <133604702+3umaGH@users.noreply.github.com>
2023-11-27 20:46:25 +0200

    feat: login page mockup

---
+++ src/pages/LoginPage.tsx
@@ -0,0 +1,50 @@
+import { Button, Divider, Paper, Typography } from "@mui/material";
+import { Link } from "react-router-dom";
+
+export const LoginPage = () => {
+  return (
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        alignItems: "center",
+        backgroundColor: "#e3e3e3",
+        height: "100vh",
+      }}
+    >
+      <img src="https://image-placeholder.com/images/image-placeholder.png" style={{width:"100%", maxWidth: "600px"}} />
+
+      <Paper
+        sx={{
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "center",
+          alignItems: "center",
+          p: 3,
+          borderRadius: "25px",
+          maxWidth: "300px",
+          mb: 20,
+          mt: 4,
+          mx:2,
+        }}
+      >
+        <Typography variant="h6" sx={{ mb: 2 }}>
+          Login to {import.meta.env.VITE_APP_PROJECT_NAME}
+        </Typography>
+
+        <Button component={Link} to={"/main"} variant="contained">
+          Sign in with Google
+        </Button>
+
+        <Divider sx={{ width: "60%", my: 2 }} />
+
+        <Typography align="center" variant="caption">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit.
+          VoluptatibusLorem ipsum dolor sit amet consectetur adipisicing elit.
+          Voluptatibus
+        </Typography>
+      </Paper>
+    </div>
+  );
+};
