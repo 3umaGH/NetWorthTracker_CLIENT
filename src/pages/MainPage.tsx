652947d446735e1d2b38550f32241b42f91e1dc9
3umaGH <133604702+3umaGH@users.noreply.github.com>
2023-11-27 20:51:10 +0200

    feat: main div

---
+++ src/pages/MainPage.tsx
@@ -0,0 +1,16 @@
+export const MainPage = () => {
+  return (
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        alignItems: "center",
+        backgroundColor: "#e3e3e3",
+        height: "100vh",
+      }}
+    >
+      hi
+    </div>
+  );
+};
