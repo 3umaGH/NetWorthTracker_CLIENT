28d1dab3da1d2b81dfe25c8ad189133131f636db
3umaGH <133604702+3umaGH@users.noreply.github.com>
2023-11-28 15:26:34 +0200

    feat: separate component

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,9 +1,10 @@
-import { Container, Grid, Box, Typography, Divider } from "@mui/material";
+import { Container, Grid, Box } from "@mui/material";
 import { AssetAllocationChart } from "../components/charts/AssetAllocationChart";
 import { NetWorthSnapshotTable } from "../components/tables/NetWorthSnapshotTable";
 import { AssetsTable } from "../components/tables/AssetsTable";
 import { FiatAssetsTable } from "../components/tables/FiatAssetsTable";
 import { CellTitle } from "../components/CellTitle";
+import { BalanceFooter } from "../components/BalanceFooter";
 
 export const MainPage = () => {
   return (
@@ -42,28 +43,7 @@ export const MainPage = () => {
           xs={12}
           sx={{ maxHeight: { xs: "20vh", md: "5vh" }, mb: { xs: 4, md: 0 } }}
         >
-          <Box textAlign={"center"}>
-            <Divider />
-            <Typography
-              variant="h2"
-              color={"black"}
-              fontSize={28}
-              fontWeight={700}
-            >
-              <Grid container>
-                <Grid item xs={12} md={3} sx={{ backgroundColor: "#b8fff3" }}>
-                  <span>1€ = 1.0941</span>
-                </Grid>
-                <Grid item xs={12} md={4.5} sx={{ backgroundColor: "#b8ffbc" }}>
-                  <span>Total (USD): 1.203M (-1231)</span>
-                </Grid>
-                <Grid item xs={12} md={4.5} sx={{ backgroundColor: "#ffb8c2" }}>
-                  <span>Total (EUR): €1.203M (-€1231)</span>
-                </Grid>
-              </Grid>
-            </Typography>
-            <Divider />
-          </Box>
+          <BalanceFooter />
         </Grid>
       </Grid>
     </Box>
