8f470c5be67688c21bf952211827a6bcca9c588b
3umaGH <133604702+3umaGH@users.noreply.github.com>
2023-11-28 15:26:46 +0200

    feat: table formatting

--- src/components/tables/NetWorthSnapshotTable.tsx
+++ src/components/tables/NetWorthSnapshotTable.tsx
@@ -1,106 +1,129 @@
-import { DataGrid, GridColDef } from "@mui/x-data-grid";
+import { Box } from "@mui/material";
+import { DataGrid, GridColDef, GridRenderCellParams } from "@mui/x-data-grid";
+import { currencySymbol } from "../../constants";
+import { formatBTC, formatEUR, formatTimeMillis, formatUSD, getColor } from "../../util";
 
 export const NetWorthSnapshotTable = () => {
   const rows = [
     {
       id: 1,
-      dateTime: "16/27/2043",
-      btcPrice: "Infinity",
-      eurUSD: "1.09",
-      totalEUR: "1.2405M",
-      totalUSD: "1.4503M",
-      totalBTC: "15.350 ₿",
+      dateTime: 53245623634,
+      btcPrice: 589452,
+      eurUSD: 1.09,
+      totalEUR: 12956165,
+      changeEUR: 1503,
+      totalUSD: 14113125.85,
+      changeUSD: -1503,
+      totalBTC: 15.353456454,
       note: "Bought a space ship",
     },
     {
       id: 2,
-      dateTime: "08/15/2023",
-      btcPrice: "45000",
-      eurUSD: "1.12",
-      totalEUR: "950K",
-      totalUSD: "1.064M",
-      totalBTC: "23.801 ₿",
+      dateTime: 2135132456234,
+      btcPrice: 45000,
+      eurUSD: 1.12,
+      totalEUR: 12956165,
+      changeEUR: -1503,
+      totalUSD: 14524262.5,
+      changeUSD: 1503,
+      totalBTC: 23.801,
       note: "Investment portfolio",
     },
     {
       id: 3,
-      dateTime: "05/10/2023",
-      btcPrice: "55000",
-      eurUSD: "1.15",
-      totalEUR: "1.750M",
-      totalUSD: "2.008M",
-      totalBTC: "18.327 ₿",
-      note: "Cryptocurrency trade",
-    },
-    {
-      id: 4,
-      dateTime: "12/03/2022",
-      btcPrice: "60000",
-      eurUSD: "1.08",
-      totalEUR: "3.120M",
-      totalUSD: "3.364M",
-      totalBTC: "27.502 ₿",
+      dateTime: 14253524,
+      btcPrice: 52000,
+      eurUSD: 1.15,
+      totalEUR: 17500000,
+      changeEUR: 2300,
+      totalUSD: 20148000,
+      changeUSD: 2300,
+      totalBTC: 18.75,
       note: "Real estate investment",
     },
     {
+      id: 4,
+      dateTime: 6534654362,
+      btcPrice: 48000,
+      eurUSD: 1.18,
+      totalEUR: 9800000,
+      changeEUR: -1200,
+      totalUSD: 11544000,
+      changeUSD: -1200,
+      totalBTC: 12.5,
+      note: "Cryptocurrency diversification",
+    },
+    {
       id: 5,
-      dateTime: "02/18/2023",
-      btcPrice: "48000",
-      eurUSD: "1.10",
-      totalEUR: "2.500M",
-      totalUSD: "2.750M",
-      totalBTC: "20.103 ₿",
-      note: "Stock market purchase",
+      dateTime: 1432523445123,
+      btcPrice: 55000,
+      eurUSD: 1.14,
+      totalEUR: 15500000,
+      changeEUR: 3000,
+      totalUSD: 17670000,
+      changeUSD: 3000,
+      totalBTC: 20,
+      note: "Gold and precious metals",
     },
     {
       id: 6,
-      dateTime: "09/05/2023",
-      btcPrice: "42000",
-      eurUSD: "1.18",
-      totalEUR: "1.950M",
-      totalUSD: "2.301M",
-      totalBTC: "14.720 ₿",
-      note: "Hedging strategy",
+      dateTime: 5324152234,
+      btcPrice: 60000,
+      eurUSD: 1.08,
+      totalEUR: 21000000,
+      changeEUR: 1800,
+      totalUSD: 22680000,
+      changeUSD: 1800,
+      totalBTC: 35,
+      note: "Tech stock investment",
     },
     {
       id: 7,
-      dateTime: "11/22/2022",
-      btcPrice: "58000",
-      eurUSD: "1.05",
-      totalEUR: "2.800M",
-      totalUSD: "2.940M",
-      totalBTC: "23.004 ₿",
-      note: "Future planning",
+      dateTime: 23452345523452,
+      btcPrice: 51000,
+      eurUSD: 1.2,
+      totalEUR: 14000000,
+      changeEUR: -2500,
+      totalUSD: 16800000,
+      changeUSD: -2500,
+      totalBTC: 22,
+      note: "Global bond market",
     },
     {
       id: 8,
-      dateTime: "07/08/2023",
-      btcPrice: "51000",
-      eurUSD: "1.14",
-      totalEUR: "1.650M",
-      totalUSD: "1.868M",
-      totalBTC: "17.205 ₿",
-      note: "Retirement fund",
+      dateTime: 234523445234,
+      btcPrice: 48000,
+      eurUSD: 1.22,
+      totalEUR: 10000000,
+      changeEUR: 1200,
+      totalUSD: 12200000,
+      changeUSD: 1200,
+      totalBTC: 18.5,
+      note: "Sustainable energy investments",
     },
     {
       id: 9,
-      dateTime: "03/12/2023",
-      btcPrice: "47000",
-      eurUSD: "1.20",
-      totalEUR: "2.200M",
-      totalUSD: "2.464M",
-      totalBTC: "19.603 ₿",
-      note: "Educational expenses",
+      dateTime: 6325463456234,
+      btcPrice: 55000,
+      eurUSD: 1.18,
+      totalEUR: 16500000,
+      changeEUR: 2700,
+      totalUSD: 19470000,
+      changeUSD: 2700,
+      totalBTC: 28,
+      note: "Precious metals and commodities",
     },
     {
       id: 10,
-      dateTime: "01/30/2023",
-      btcPrice: "52000",
-      eurUSD: "1.16",
-      totalEUR: "1.400M",
-      totalUSD: "1.624M",
-      totalBTC: "16.005 ₿",
-      note: "Vacation planning",
+      dateTime: 632456345632,
+      btcPrice: 50000,
+      eurUSD: 1.25,
+      totalEUR: 12000000,
+      changeEUR: -1800,
+      totalUSD: 15000000,
+      changeUSD: -1800,
+      totalBTC: 20.5,
+      note: "Real estate development",
     },
   ];
 
@@ -172,10 +195,92 @@ export const NetWorthSnapshotTable = () => {
     },
   ];
 
+  const cellRenderer = (params: GridRenderCellParams) => {
+    const { field, value } = params;
+
+    console.log(params.row);
+
+    switch (field) {
+      case "dateTime":
+        return (
+          <Box
+            sx={{
+              color: "#575757",
+              fontWeight: "500",
+            }}
+          >
+            {formatTimeMillis(value)}
+          </Box>
+        );
+      case "btcPrice":
+        return (
+          <Box
+            sx={{
+              color: "#f57e0f",
+              fontWeight: "500",
+            }}
+          >
+            {currencySymbol}
+            {value}
+          </Box>
+        );
+      case "eurUSD":
+        return (
+          <Box
+            sx={{
+              color: "#2aa5b0",
+              fontWeight: "500",
+            }}
+          >
+            {currencySymbol}
+            {value}
+          </Box>
+        );
+      case "totalEUR":
+        return (
+          <Box
+            sx={{
+              color: getColor(params.row.changeEUR),
+              fontWeight: "500",
+            }}
+          >
+            {`${formatEUR(value)} (${formatEUR(params.row.changeEUR, 0)})`}
+          </Box>
+        );
+      case "totalUSD":
+        return (
+          <Box
+            sx={{
+              color: getColor(params.row.changeUSD),
+              fontWeight: "500",
+            }}
+          >
+            {`${formatUSD(value)} (${formatUSD(params.row.changeUSD, 0)})`}
+          </Box>
+        );
+      case "totalBTC":
+        return (
+          <Box
+            sx={{
+              color: "#f57e0f",
+              fontWeight: "500",
+            }}
+          >
+            {formatBTC(value)}
+          </Box>
+        );
+      default:
+        return value;
+    }
+  };
+
   return (
     <DataGrid
       rows={rows}
-      columns={columns}
+      columns={columns.map((column) => ({
+        ...column,
+        renderCell: cellRenderer,
+      }))}
       hideFooter={true}
       disableRowSelectionOnClick
       density={"compact"}
