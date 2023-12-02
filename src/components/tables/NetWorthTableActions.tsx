73be7d6cac14bbb2eea7c878547b20c164855fb6
3umaGH <133604702+3umaGH@users.noreply.github.com>
2023-12-02 18:38:31 +0200

    feat: added limit for snapshots

--- src/components/tables/NetWorthTableActions.tsx
+++ src/components/tables/NetWorthTableActions.tsx
@@ -15,18 +15,26 @@ export const NetWorthTableActions = ({
     <Box sx={{ "& button": { m: 0, p: 0, minWidth: "30px" } }}>
       {rowID === totalRows && (
         <div>
-          <Tooltip title="Create new snapshot">
+          {!(totalRows >= 1000) && (
+            <Tooltip title="Create new snapshot">
+              <Button
+                variant="text"
+                color="success"
+                sx={{ fontSize: 18, p: 0, m: 0 }}
+                onClick={() => dispatch(addSnapshot())}
+              >
+                🖬
+              </Button>
+            </Tooltip>
+          )}
+
+          <Tooltip title="Delete this snapshot">
             <Button
               variant="text"
-              color="success"
-              sx={{ fontSize: 18, p: 0, m: 0 }}
-              onClick={() => dispatch(addSnapshot())}
+              color="error"
+              sx={{ fontSize: 18 }}
+              onClick={() => dispatch(deleteSnapshot(rowID))}
             >
-              🖬
-            </Button>
-          </Tooltip>
-          <Tooltip title="Delete this snapshot">
-            <Button variant="text" color="error" sx={{ fontSize: 18 }} onClick={() => dispatch(deleteSnapshot(rowID))}>
               X
             </Button>
           </Tooltip>
