369fb6af5b62135e8e0b60a28972d2b77ed99412
3umaGH <133604702+3umaGH@users.noreply.github.com>
2023-12-03 17:13:20 +0200

    removed debug message

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -26,7 +26,6 @@ export const LoginPage = () => {
 
   useEffect(() => {
     FirebaseAuth.currentUser ? navigate("/main") : null;
-    console.log("cur", FirebaseAuth.currentUser);
   }, [isUserUpdated]);
 
   return (
