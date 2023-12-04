edc88010a57f639ac82a7f76f48795a2ea7ac394
3umaGH <133604702+3umaGH@users.noreply.github.com>
2023-12-04 22:41:33 +0200

    feat: reworked protected route to prevent unnecessary redirects

--- src/components/ProtectedRoute.tsx
+++ src/components/ProtectedRoute.tsx
@@ -1,13 +1,18 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { FirebaseAuth } from "../firebase/firebase";
 import { useNavigate } from "react-router-dom";
 
 export const ProtectedRoute = ({ children }: { children: React.ReactNode }) => {
+  const [refresh, setRefresh] = useState(0);
   const navigate = useNavigate();
 
+  // Give firebase some time to fetch current user and then check if user is actually logged in.
   useEffect(() => {
-    if (!FirebaseAuth.currentUser) return navigate("/");
+    setTimeout(() => {
+      if (!FirebaseAuth.currentUser) return navigate("/");
+      setRefresh(refresh + 1);
+    }, 500);
   });
 
-  return children;
+  if (FirebaseAuth.currentUser) return children;
 };
